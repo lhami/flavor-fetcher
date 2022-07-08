cbb9d09981e54489696642843b9ffa7dea1b343a
Leah Hamilton <leah.hamilton0@gmail.com>
2022-07-08 09:44:43 -0400

    Adding files previously worked on

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
-# flavor-fetcher
-Python code to use a pre-trained LSTM for identifying flavor-descriptors in natural language English text. See https://doi.org/10.3390/foods10071633 for details.
+# Sensory Descriptor Identifier
+
+This tool was created by Chreston Miller, a researcher at Virginia Tech, to identify words with flavor-descriptive meanings in food reviews and other texts. It is based on the archetecture described by Intuition Engineering in the article ["Deep learning for specific information extraction from unstructured texts"](https://towardsdatascience.com/deep-learning-for-specific-information-extraction-from-unstructured-texts-12c5b9dceada). The results on whiskey reviews have been published in ["Sensory Descriptor Analysis of Whisky Lexicons through the Use of Deep Learning"](https://doi.org/10.3390/foods10071633).
+
+Programmed in Python using the deep learning backend keras, this package includes a set of whiskey reviews from [Whiskycast](https://whiskycast.com/tastingnotes/), [WhiskyAdvocate](https://www.whiskyadvocate.com/ratings-and-reviews/), [The Whiskey Jug](https://thewhiskeyjug.com/), and [Breaking Bourbon](https://www.breakingbourbon.com/bourbon-rye-whiskey-reviews-sort-by-review-date) with manually-annotated examples of flavor-descriptive and flavor-nondescriptive adjectives and nouns, code to train a neural network to predict the descriptive or nondescriptive status of a token in context, the trained network itself, and code to use the trained network to tag all tokens in the example dataset as descriptive or nondescriptive.
+
+## Usage
+
+This code is intended to replicate the results from ["Sensory Descriptor Analysis of Whisky Lexicons through the Use of Deep Learning"](https://doi.org/10.3390/foods10071633). If you wish to update the code to train a new predictor or make and test predictions on a new set of data, you will have to make modifications to the code itself. We plan to convert this repository to R code in the future.
+
+## Files
+- `README.md` - This file.
+- `100_2021-04-20 10_38_24.203316_3_epochs_re-run_model.h5` - The exact network trained and used in the paper ["Sensory Descriptor Analysis of Whisky Lexicons through the Use of Deep Learning"](https://doi.org/10.3390/foods10071633). Necessary to make predictions using `CollectPredictions.py` or `CollectPredictions.ipynb`.
+- `annotated_whisky_reviews_032320.csv` - A data file containing one row per token in the [Whiskycast](https://whiskycast.com/tastingnotes/) and [WhiskyAdvocate](https://www.whiskyadvocate.com/ratings-and-reviews/) reviews, used for prediction. Tokenization and annotation done using SpaCy through CleanNLP. Only the fields `upos`, `token`, `doc_id`, `sid`, and `tid` are used.
+- `CollectPredictions.py` - A python script that, given a pretrained model file (`*.h5`), a copy of GloVe embeddings (`glove.42B.300d.zip`), and a set of tokenized review data as a one-line-per-token `.csv` (with columns `upos`, `token`, `doc_id`, `sid`, and `tid`, as output from e.g. SpaCy), will output a prediction for each individual token in context (0-1, with 0 being nondescriptive and 1 being descriptive).
+- `CollectPredictions.ipynb` - A jupyter notebook version of the code in `CollectPredictions.py`.
+- `descriptor_model.py` - A python script defining the `DescriptorNN` class, which holds a (trained) descriptor-predictor model and is used to make predictions.
+- `train.py` - A python script to train a `DescriptorNN` based on a `.csv` containing examples of annotated tokens in context (Note: the version currently here is out of date, I need to email Dr. Miller)
+- `library.py` - Various helper functions used in `CollectPredictions.*` and `descriptor_model.py`
+- `wawc_whiskey_reviews_tokenwise_lstm_predictions.csv` - A data file containing the output of `CollectPredictions.*` when used to make predictions on `annotated_whisky_reviews_032320.csv`
---
+++ src/descriptor_model.py
@@ -0,0 +1,139 @@
+import keras
+from keras.callbacks import ModelCheckpoint
+from keras.utils.np_utils import to_categorical
+import numpy as np
+from matplotlib import pyplot #only necessary to train the model, could add a toggle
+
+#from library import * #Previously, this file was calling library, and now library (renamed to ExtractDescriptors) calls this file?
+
+#Script to build the descriptor extractor model and run fit and predict given specified input.
+
+#courtesy of: https://towardsdatascience.com/deep-learning-for-specific-information-extraction-from-unstructured-texts-12c5b9dceada
+#Currently creating a model that takes in the input phrase and its context
+class DescriptorExtractorNN:
+
+    #def __init__(self, word_features_dim, dense_features_dim):
+    #dense_features_dim wasn't actually being used
+    def __init__(self, word_features_dim):
+
+        self.dim = word_features_dim
+
+
+        lstm_input_phrase = keras.layers.Input(shape=(1, word_features_dim))
+        lstm_input_cont = keras.layers.Input(shape=(None, word_features_dim))
+
+        lstm_emb_phrase = keras.layers.LSTM(256)(lstm_input_phrase)
+        lstm_emb_phrase = keras.layers.Dense(128, activation='relu')(lstm_emb_phrase)
+
+        lstm_emb_cont = keras.layers.LSTM(256)(lstm_input_cont)
+        lstm_emb_cont = keras.layers.Dense(128, activation='relu')(lstm_emb_cont)
+
+        x = keras.layers.concatenate([lstm_emb_phrase, lstm_emb_cont])
+        x = keras.layers.Dense(128, activation='relu')(x)
+        x = keras.layers.Dense(64, activation='relu')(x)
+        x = keras.layers.Dense(32, activation='relu')(x)
+
+        main_output = keras.layers.Dense(2, activation='softmax')(x) #softmax is meant to be used for classification with greater than 2 ourputs
+        #main_output = keras.layers.Dense(2, activation='sigmoid')(x) #works for classification with 2 outputs
+
+        self.model = keras.models.Model(inputs=[lstm_input_phrase, lstm_input_cont],
+                                        outputs=main_output)
+
+        optimizer = keras.optimizers.Adam(lr=0.0001)
+
+        self.model.compile(optimizer=optimizer, loss='binary_crossentropy',metrics=['accuracy'])
+        
+    def fit(self, lstm_phrase, lstm_precontext, lstm_postcontext, y,
+            val_split=0.2, patience=5, max_epochs=1000, batch_size=32):
+        
+        #==========================================
+         # Preparing phrase
+        lstm_phrase = keras.preprocessing.sequence.pad_sequences(lstm_phrase, padding='pre', dtype='float32')
+        lstm_phrase = np.reshape(lstm_phrase, (len(lstm_phrase), 1, self.dim))
+        
+        # Preparing context
+        lstm_padded_pre_context = keras.preprocessing.sequence.pad_sequences(lstm_precontext, padding='pre', dtype='float32')
+        lstm_padded_post_context = keras.preprocessing.sequence.pad_sequences(lstm_postcontext, padding='post', dtype='float32')
+        lstm_context = np.concatenate((lstm_padded_pre_context,lstm_padded_post_context), axis=1);                
+        lstm_context = np.reshape(lstm_context, (len(lstm_context),6, self.dim))                
+
+        # Preparing labels
+        y_onehot = to_categorical(y)
+        #==========================================
+        
+        # define the checkpoint: I think this saves the weights AND the model
+        filepath="./checkpoints/weights-improvement-{epoch:02d}-{loss:.4f}-{accuracy:.4f}.hdf5"
+        checkpoint = ModelCheckpoint(filepath, monitor='loss', verbose=1, save_best_only=True, mode='min')
+        callbacks_list = [checkpoint]
+		
+        hist = self.model.fit([lstm_phrase, lstm_context],
+                       y_onehot,
+                       batch_size=batch_size,
+                       epochs=max_epochs,
+                       validation_split=val_split,
+                       callbacks=[keras.callbacks.EarlyStopping(monitor='accuracy', patience=patience),checkpoint])
+
+        print("----------------------------------")               
+        print(hist.history)
+        print("----------------------------------")
+
+        return hist
+
+        # plot train and validation loss
+        pyplot.plot(hist.history['loss'])
+        pyplot.plot(hist.history['val_loss'])
+        pyplot.title('Model train vs validation loss')
+        pyplot.ylabel('Loss')
+        pyplot.xlabel('Epoch')
+        pyplot.legend(['Train', 'Validation'], loc='upper right')
+        pyplot.show()
+        #print("Accuracy:",np.mean(hist.history['accuracy']))
+    #===========================================
+    
+    def evaluate(self, lstm_phrase, lstm_precontext, lstm_postcontext, y, patience=5, batch_size=32):
+
+        # Preparing phrase
+        lstm_phrase = keras.preprocessing.sequence.pad_sequences(lstm_phrase, padding='pre', dtype='float32')
+        lstm_phrase = np.reshape(lstm_phrase, (len(lstm_phrase), 1, self.dim))
+
+        # Preparing context
+        lstm_padded_pre_context = keras.preprocessing.sequence.pad_sequences(lstm_precontext, padding='pre', dtype='float32')
+        lstm_padded_post_context = keras.preprocessing.sequence.pad_sequences(lstm_postcontext, padding='post', dtype='float32')
+        lstm_context = np.concatenate((lstm_padded_pre_context,lstm_padded_post_context), axis=1);
+        lstm_context = np.reshape(lstm_context, (len(lstm_context),6, self.dim))
+
+        # Preparing labels
+        y_onehot = to_categorical(y)
+
+        score = self.model.evaluate([lstm_phrase, lstm_context], y_onehot, batch_size=batch_size)
+
+        print(self.model.metrics_names)
+        print(score)
+    #===========================================
+                       
+    def predict(self, lstm_phrase, lstm_precontext, lstm_postcontext):
+        
+        # Preparing phrase
+        lstm_phrase = keras.preprocessing.sequence.pad_sequences(lstm_phrase, padding='pre', dtype='float32')
+        lstm_phrase = np.reshape(lstm_phrase, (len(lstm_phrase), 1, self.dim))
+        
+        # Preparing context
+        lstm_padded_pre_context = keras.preprocessing.sequence.pad_sequences(lstm_precontext, padding='pre', dtype='float32')
+        lstm_padded_post_context = keras.preprocessing.sequence.pad_sequences(lstm_postcontext, padding='post', dtype='float32')
+        lstm_context = np.concatenate((lstm_padded_pre_context,lstm_padded_post_context), axis=1);
+        lstm_context = np.reshape(lstm_context, (len(lstm_context),6, self.dim))
+
+        y = self.model.predict([lstm_phrase, lstm_context])
+
+        return y
+    
+    def onehot_transform(self,y):
+
+        onehot_y = []
+    
+        for numb in y:
+            onehot_arr = np.zeros(2)
+            onehot_arr[numb] = 1
+            onehot_y.append(np.array(onehot_arr))
+    
+        return np.array(onehot_y)
